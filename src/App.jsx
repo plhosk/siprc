f4b2bfb4aa972566f16ce89355a5a4594eead4d8
Paul Hoskinson <plhosk@gmail.com>
2017-10-11 22:33:28 -0700

    tweaks to style

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import './App.css'
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <div className="App-header-img-div" />
-      { /* <img className="App-img-bird" width="100%" src="bird-rock.jpg" alt="bird on rock" /> */ }
-      <div className="App-logo-text">SIPRC</div>
-      <h1 className="App-title">Saturna Island Parks and Recreation Commission</h1>
+      <img className="App-header-img" width="100%" src="bird-rock-cropped.jpg" alt="bird on rock" />
+      <div className="App-header-title">
+        <div className="App-logo-text">SIPRC</div>
+        <h1 className="App-title">Saturna Island Parks and Recreation Commission</h1>
+      </div>
     </header>
     <nav className="App-nav">
       <div>Parks</div>
@@ -17,22 +18,18 @@ const App = () => (
       <div>About</div>
     </nav>
     <main>
-      <p className="App-intro">
-        Welcome to the website of the Saturna Island Parks and Recreation Commission.
-      </p>
-      <p className="App-intro">
-        This website is under construction. Thank you for your patience.
-      </p>
-      { /*
+      <div className="App-intro-div">
+        <p className="App-intro">
+          Welcome to the website of the Saturna Island Parks and Recreation Commission.
+        </p>
+        <p className="App-intro">
+          This website is under construction. Thank you for your patience.
+        </p>
+      </div>
       <section>
-        <h2>Parks</h2>
-        <p>Here is some information about parks on Saturna Island.</p>
+        <h2>Contact</h2>
+        <p>Email us at: <a href="mailto:parksrec@saturnanet.net">parksrec@saturnanet.net</a></p>
       </section>
-      <section>
-        <h2>Trails</h2>
-        <p>Here is some information about trails on Saturna Island.</p>
-      </section>
-      */ }
     </main>
   </div>
 )
