b3a196ac82123b7372c2a8f47e7af2a379dbb805
Paul Hoskinson <plhosk@gmail.com>
2018-12-17 18:30:01 -0800

    add community survey 2018

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,10 +10,24 @@ const Home = () => (
       {/* <p className="App-intro">
         Click the blue tabs to continue.
       </p> */}
-      <p className="App-intro">
+      {/* <p className="App-intro">
         This website is under construction. Thank you for your patience.
-      </p>
+      </p> */}
     </div>
+
+    <h3>2018 SIPRC Community Survey</h3>
+
+    The results of the 2018 SIPRC Community Survey are in.
+    <br />
+    <br />
+    View the presentation here: &nbsp;
+    <a href="/docs/siprc-community-survey-2018.pdf" target="_blank">
+      PDF format (.pdf)
+    </a>
+    &nbsp;&nbsp;
+    <a href="/docs/siprc-community-survey-2018.pptx" target="_blank">
+      PowerPoint format (.pptx)
+    </a>
 
   </section>
 )
